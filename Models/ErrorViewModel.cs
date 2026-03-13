874ad86b7b890d34ad6ac813a35335b7dad87a9d
Quang Lùn <quangspinner12@gmail.com>
2026-03-14 02:17:37 +0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BAO_Cinemas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

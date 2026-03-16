f3ba8246cab747eb001a5a12c237bf1896699c70
Quang Lùn <quangspinner12@gmail.com>
2026-03-16 20:51:49 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BAO_Cinemas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

d1d4a8b6a2628afb9a768ee311eac2915d6e4788
Quang Lùn <quangspinner12@gmail.com>
2026-03-16 20:56:45 +0700

    Add files via upload

---
+++ BAO_Cinemas/Models/Cinema.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BAO_Cinemas.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } // Tên rạp (VD: BAO Cinemas Cầu Giấy)
+
+        public string Address { get; set; } // Địa chỉ rạp
+
+        public string Hotline { get; set; } // Số điện thoại
+
+        // Mối quan hệ: 1 Rạp sẽ có nhiều Phòng chiếu
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
